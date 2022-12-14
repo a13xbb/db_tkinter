e072b182e4794c039b122a42570170003259be71
a13xbb <alex.blokh24@gmail.com>
2022-12-14 18:00:09 +0300

    back button working

--- app.py
+++ app.py
@@ -14,9 +14,12 @@ class App(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
-        start_page = StartPage(parent=container, controller=self)
-        start_page.place(relwidth=1, relheight=1)
-        start_page.tkraise()
+        self.start_page = StartPage(parent=container, controller=self)
+        self.start_page.place(relwidth=1, relheight=1)
+        self.show_start_page()
+
+    def show_start_page(self):
+        self.start_page.tkraise()
 
 
 class StartPage(tk.Frame):
@@ -24,6 +27,7 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg='gray')
         self.controller = controller
+        self.parent = parent
         label = tk.Label(self, text="Sign in or sign up", bg='gray', font=25)
         label.pack(side="top", fill="x", pady=10)
 
@@ -35,7 +39,7 @@ class StartPage(tk.Frame):
         button2.pack()
 
     def redirect_to_admin_login_page(self):
-        admin_login = AdminLogin(parent=self, controller=self.controller)
+        admin_login = AdminLogin(parent=self.parent, controller=self.controller)
         admin_login.place(relwidth=1, relheight=1)
         admin_login.tkraise()
 
@@ -45,6 +49,7 @@ class AdminLogin(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg='gray')
         self.controller = controller
+        self.parent = parent
         label = tk.Label(self, text="Sign in as superuser", bg='gray', font=25)
         label.pack(side="top", fill="x", pady=10)
 
@@ -61,7 +66,7 @@ class AdminLogin(tk.Frame):
     def redirect_to_admin_page(self, username, password):
         try:
             conn = login(username, password)
-            admin_page = AdminPage(parent=self, connection=conn, controller=self.controller,
+            admin_page = AdminPage(parent=self.parent, connection=conn, controller=self.controller,
                                    username=username, password=password)
             admin_page.place(relwidth=1, relheight=1)
             admin_page.tkraise()
@@ -74,6 +79,7 @@ class AdminPage(tk.Frame):
     def __init__(self, parent, connection, controller, username, password):
         tk.Frame.__init__(self, parent, bg='gray')
         self.controller = controller
+        self.parent = parent
         self.conn = connection
         self.username = username
         self.password = password
@@ -88,10 +94,14 @@ class AdminPage(tk.Frame):
         button_delete = tk.Button(self, text="Delete user database", font=25, bg='#2bbcd4',
                                   command=lambda: drop_db('user_db', self.username, self.password))
         button_delete.pack()
-
-        button_back = tk.Button(self, text="Back", font=25)
+        print(type(controller.show_start_page))
+        button_back = tk.Button(self, text="Back", font=25,
+                                command=controller.show_start_page)
         button_back.pack()
 
     # def go_back(self):
-    #     self.start_page.place(relwidth=1, relheight=1)
-    #     self.start_page.tkraise()
+    #     print('in here')
+    #     sp = StartPage(parent=self, controller=self.controller)
+    #     sp.place(relwidth=1, relheight=1)
+    #     sp.tkraise()
+    #     print('in here 2')
